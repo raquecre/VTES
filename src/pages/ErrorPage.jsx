23d8ec20aca6f931a2b244f262ed1abff370f74f
raquecre <raquecre@gmail.com>
2023-10-02 18:29:33 +0200

    [feature-24] create error page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,28 @@
+
+import { Link } from "react-router-dom";
+import Header from "../Components/Header";
+
+
+const ErrorPage = () => {
+
+
+    return (
+        <div className="bg-gradient-to-r from-emerald-800/90 to-teal-800/90 bg-fixed ">
+            <Header />
+            <div className="flex flex-col items-center">
+                <img src="https://i.imgur.com/IlxasbV.png" alt="error" srcset="" />
+
+                <Link className="bg-black text-white rounded-full p-2 text-2xl w-fit" to="/">
+                    Vuelve a Home
+                </Link>
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+
+export default ErrorPage;
