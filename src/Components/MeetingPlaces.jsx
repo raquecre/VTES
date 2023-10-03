67500ffad7cff0f2556095b63dec12a24025cc7e
raquecre <raquecre@gmail.com>
2023-10-03 13:27:17 +0200

    [feature-27] re-reduce div map

--- src/Components/MeetingPlaces.jsx
+++ src/Components/MeetingPlaces.jsx
@@ -16,7 +16,7 @@ const MeetingPlaces = () => {
 
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.252474026776!2d-3.706941823248376!3d40.403257371441754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4227d299d05583%3A0xf79c33a6b4bff502!2sCalle%20de%20las%20Pe%C3%B1uelas%2C%2014%2C%2028005%20Madrid!5e0!3m2!1ses!2ses!4v1694105207814!5m2!1ses!2ses"
                     className="rounded-xl
-                    xl:w-[600px] xl:h-[400px]  w-[500px] h-[300px]"
+                    xl:w-[600px] xl:h-[400px] lg:w-[600px] lg:h-[400px] md:w-[600px] md:h-[400px] w-[270px] h-[170px]"
                     allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
                 <div className= "grid items-center align-center content-center gap-5 pt-5">
