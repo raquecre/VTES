6ab2bc7f0176d9630a8190da3b318dc2fa1a82fb
raquecre <raquecre@gmail.com>
2023-10-18 12:13:29 +0200

    [feature-29]-prototype style journal

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -22,7 +22,6 @@ const Calendar = () => {
                         src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%230B8043&ctz=Europe%2FMadrid&showTitle=0&showDate=0&showPrint=0&showNav=1&showCalendars=0&showTz=0&src=ZTIxNmZmNmY5NGYzMjE1N2Y1NjA3ZTRhMzkzMDNkODcxODZlMzYyYmYwYjE5ZjAxZDM4NjgyYmZhOWZhZmFhZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F6BF26"></iframe>
 
                     <div className="w-1/4 m-3">
-                        <H1Titles h1Title="agenda" />
                         <EventsList />
                     </div>
                 </div>
--- src/Components/Journal.jsx
+++ src/Components/Journal.jsx
@@ -2,9 +2,22 @@ const EventsList = () => {
 
 
     return (
-        <div className="">
-            <h3 className="text-lg font-semibold bg-amber-600 p-1 m-2 rounded-lg">Noche de los primogénitos</h3>
-            <h3 className="text-lg font-semibold bg-lime-700 p-1 rounded-lg">Partidas abiertas</h3>
+        <div className="m-2 ">
+            <h3 className="text-lg font-bold bg-amber-600/90 p-1 m-2 rounded-lg">Noche de los primogénitos</h3>
+            <p className="text-md bg-amber-600/60 p-2 m-3 rounded-lg">
+                En la noche solo hay presas y depredadores y te encuentras en un limbo peligroso.
+                Tu poder como primogénito te permite cazar humanos, pero hay cosas claramente más peligrosas acechando en las sombras...
+        </p>
+
+            <h4 className="text-lg font-semibold bg-amber-600 p-1 m-2 rounded-lg">Próxima partida: 22 de octubre</h4>
+
+            <h3 className="text-lg font-bold bg-lime-600/90 p-1 mt-5  m-2 rounded-lg">Partidas abiertas</h3>
+            <p className="text-md bg-lime-500/40 p-2 m-3 rounded-lg">
+                En la noche solo hay presas y depredadores y te encuentras en un limbo peligroso.
+                Tu poder como primogénito te permite cazar humanos, pero hay cosas claramente más peligrosas acechando en las sombras...
+                Te ofrecemos la posibilidad de unirte a nosotros para que, tal vez, aguantes una noche más y sigas cazando a los débiles.</p>
+           
+            <h4 className="text-lg font-semibold bg-lime-600 p-1 m-2 rounded-lg">Próxima partida: 22 de octubre</h4>
 
         </div>
     )
