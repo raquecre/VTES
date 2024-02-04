bed3f2a0e3abaa47c0e5d9adc778ce371dffc8ef
raquecre <raquecre@gmail.com>
2024-02-04 21:38:25 +0100

    league february

--- src/Components/data/TotalPlayers.jsx
+++ src/Components/data/TotalPlayers.jsx
@@ -1,25 +1,27 @@
 
 const TotalPlayers = [
+    { id: 4, player: 'Tirso', points: 3 },
+    { id: 6, player: 'Daniel', points: 2 },
+    { id: 14, player: 'Javier', points: 1.5 },
+    { id: 16, player: 'Alejandro', points: 1.5 },
+    { id: 5, player: 'Ziad', points: 1 },
+    { id: 8, player: 'Óscar David', points: 1 },
+    { id: 9, player: 'Diego', points: 1 },
+    { id: 11, player: 'Guillermo', points: 1 },
+    { id: 7, player: 'Salvador', points: 0.5 },
     { id: 1, player: 'Álvaro', points: 0 },
     { id: 2, player: 'Pablos', points: 0 },
     { id: 3, player: 'Manuel', points: 0 },
-    { id: 4, player: 'Tirso', points: 0 },
-    { id: 5, player: 'Ziad', points: 0 },
-    { id: 6, player: 'Daniel', points: 0 },
-    { id: 7, player: 'Salvador', points: 0 },
-    { id: 8, player: 'Óscar David', points: 0 },
-    { id: 9, player: 'Diego', points: 0 },
     { id: 10, player: 'Raquel', points: 0 },
-    { id: 11, player: 'Guillermo', points: 0 },
     { id: 12, player: 'Adrián', points: 0 },
     { id: 13, player: 'Esther', points: 0 },
-    { id: 14, player: 'Javier', points: 0 },
     { id: 15, player: 'Dario', points: 0 },
-    { id: 16, player: 'Alejandro', points: 0 },
     { id: 17, player: 'Jorge', points: 0 },
     { id: 18, player: 'Jose Miguel', points: 0 },
     { id: 19, player: 'Rubén', points: 0 },
 
 ];
+
+
 
 export default TotalPlayers
